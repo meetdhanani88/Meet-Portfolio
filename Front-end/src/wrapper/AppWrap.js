0b4edb011017912e6ff69117b2f5bcd7ae7c444c
C223 <c223@C223s-iMac.local>
2022-05-03 16:09:13 +0530

    new section added

--- Front-end/src/wrapper/AppWrap.js
+++ Front-end/src/wrapper/AppWrap.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavigationDots, SocialMedia } from '../components';
 
 const AppWrap = (Component, idName, classNames) => function HOC() {
+
     return (
         <div id={idName} className={`app__container ${classNames}`}>
             <SocialMedia />
